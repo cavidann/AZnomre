5882d961b5d1b0a93d83a19c65b32aef9eb6edfd
cavidan <cavi98@icloud.com>
2017-03-15 02:13:24 -1100

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,27 @@ document.getElementById('yaz').addEventListener('focus', function () {
         var x = this.value = "+994-";
     }
     document.getElementById('yaz').addEventListener('keypress', function (a) {
-        e++;        
+        e++;     
         if (e <= 9) {
             if (e == 3||e==6||e==8||e==10) {
                 var y = this.value += "-";
             }
+            if (event.which < 58 && event.which > 47) {
             return true;
+            }
+            else{
+                a.preventDefault();
+                e--;
+        return false;
+            }
         }
         a.preventDefault();
         return false;
     });
 });
+
+
+
 
 
 
